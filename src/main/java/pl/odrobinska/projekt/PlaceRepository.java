516a6348d4c6cd9535682e1c1a86f1e71225c596
KorneliaOdrobinska <kornelia.odrobinska@gmail.com>
2021-02-15 23:10:47 +0100

    Displaying records in htlm

--- src/main/java/pl/odrobinska/projekt/PlaceRepository.java
+++ src/main/java/pl/odrobinska/projekt/PlaceRepository.java
@@ -6,23 +6,55 @@ import java.util.Optional;
 
 public class PlaceRepository<T extends Place> {
     private List<T> placeElements;
+// TODO zrobic jedno globalne repozytorium z metofami findAll(String place) - ktore zwroci liste List<Kitchen||Bedroom||Hall>
+   // TODO wyrzucić szczegółowe repozytoria i zamienic uzycie Servlecie
+    /*
+    List<T extends Place> findAll(String place) {
+        var session = HibernateUtil.getSessionFactory().openSession();
+        var transaction = session.beginTransaction();
+        List<Place> result;
+        switch (place){
+            case "kitchen":
+                placeElements<Kitchen> = session.createQuery("from Kitchen", Kitchen.class).list();
+            case "bedroom":
+                result = session.createQuery("from Bedroom", Bedroom.class).list();
+            case "hall":
+                placeElements = session.createQuery("from Hall", Hall.class).list();
 
-    // TODO zastosowac wyrazenia lambda
-    Optional<T> findById(Integer id){
-        T tempItem = null;
-        for(T item: placeElements){
-            if (item.getId() == id){
-                tempItem = item;
-            }
-        }
-        return Optional.ofNullable(tempItem);
-    }
-    // TODO wybieranie wpisow z odpowiedniej tabeli
-   /*
-    List<Optional<T>> findByPlace(String place){
-        // place - np. tabela Kitchen
-        // return - wszystkie wpisy z tabeli Kitchen (place)
+        transaction.commit();
+        session.close();
+        return result;
     }
 
-    */
+     */
+
+    /*  Optional<Bedroom> findById(Integer id){
+          // TODO zastosowac wyrazenia lambda
+              Bedroom tempItem = null;
+              for(Bedroom item: bedroomElements){
+                  if (item.getId() == id){
+                      tempItem = item;
+                  }
+              }
+              return Optional.ofNullable(tempItem);
+          }
+
+     */
+    Optional<Bedroom> findById(Integer id){
+        var session = HibernateUtil.getSessionFactory().openSession();
+        var transaction = session.beginTransaction();
+        var result = session.get(Bedroom.class, id);
+        transaction.commit();
+        session.close();
+        return Optional.ofNullable(result);
+    }
+
+    Bedroom addBedroomElement(Bedroom newBedroomElement){
+        var session = HibernateUtil.getSessionFactory().openSession();
+        var transaction = session.beginTransaction();
+        session.persist(newBedroomElement);
+        transaction.commit();
+        session.close();
+        return newBedroomElement;
+    }
 }
--- src/main/java/pl/odrobinska/projekt/Servlet.java
+++ src/main/java/pl/odrobinska/projekt/Servlet.java
@@ -9,25 +9,80 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.*;
+import java.sql.*;
+import java.util.List;
 import java.util.Optional;
 
 @WebServlet(name = "Remont", urlPatterns = {"/api"})
 public class Servlet extends HttpServlet {
     private Logger logger = LoggerFactory.getLogger(Servlet.class);
-    private static final String PROJECT_PARAM = "project";
+    private static final String PROJECT_PARAM = "place";
     BedroomRepository bedroomRepository = new BedroomRepository();
     Bedroom defaultBedroomElement = new Bedroom("e","link","descr",null);
     HallRepository hallRepository = new HallRepository();
     Hall defaultHallElement = new Hall("e","link","descr",null, false);
     KitchenRepository kitchenRepository = new KitchenRepository();
     Kitchen defaultKitchenElement = new Kitchen("e","link","descr",null, Type.DARK);
-    String index = "1";
+    String place = "1";
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         logger.info("Get request with params: " + req.getParameterMap());
-       index = req.getParameter(PROJECT_PARAM);
-       resp.getWriter().write("Answer is: " + kitchenRepository.findById(Integer.parseInt(Optional.ofNullable(index).orElse("1"))).orElse(defaultKitchenElement).getDescription());
+       place = req.getParameter(PROJECT_PARAM);
+
+      // resp.getWriter().write("Answer is: " + kitchenRepository.findById(Integer.parseInt(Optional.ofNullable(place).orElse("1"))).orElse(defaultKitchenElement).getDescription());
+
+        PrintWriter out = resp.getWriter();
+        resp.setContentType("text/html");
+        out.println("<html><body>");
+        /*
+        try
+        {
+            out.println("<table border=1 width=50% height=50%>");
+            out.println("<tr><th>Element</th><th>Link</th><th>Description</th><tr>");
+            for(int index = 0; index < bedroomList.size(); index++){
+                out.println("<tr><td>" + bedroomList.get(index).getElement() + "</td><td>" + bedroomList.get(index).getLink() + "</td><td>" + bedroomList.get(index).getDescription() + "</td></tr>");
+            }
+            out.println("</table>");
+            out.println("</html></body>");
+        }
+        catch (Exception e)
+        {
+            out.println("error");
+        }
+
+         */
+
+        try
+        {
+            out.println("<table class=\"pure-table pure-table-bordered\"><thead>");
+            out.println("<tr><th>Element</th><th>Link</th><th>Author</th><tr><tbody>");
+            if (place.equals("bedroom")) {
+                List<Bedroom> bedroomList = bedroomRepository.findAll();
+                for (int index = 0; index < bedroomList.size(); index++) {
+                    out.println("<tr><td>" + bedroomList.get(index).getElement() + "</td><td><a href=\"" + bedroomList.get(index).getLink() + "\" target=\"_blank\">" + Optional.ofNullable(bedroomList.get(index).getDescription()).orElse("Link") + "</a></td><td>" + bedroomList.get(index).getAuthor() + "</td></tr>");
+                }
+            }
+            else if (place.equals("kitchen")) {
+                List<Kitchen> kitchenList = kitchenRepository.findAll();
+                for (int index = 0; index < kitchenList.size(); index++) {
+                    out.println("<tr><td>" + kitchenList.get(index).getElement() + "</td><td><a href=\"" + kitchenList.get(index).getLink() + "\" target=\"_blank\">" + Optional.ofNullable(kitchenList.get(index).getDescription()).orElse("Link") + "</a></td><td>" + kitchenList.get(index).getAuthor() + "</td></tr>");
+                }
+            }
+            else {
+                List<Hall> hallList = hallRepository.findAll();
+                for (int index = 0; index < hallList.size(); index++) {
+                    out.println("<tr><td>" + hallList.get(index).getElement() + "</td><td><a href=\"" + hallList.get(index).getLink() + "\" target=\"_blank\">" + Optional.ofNullable(hallList.get(index).getDescription()).orElse("Link") + "</a></td><td>" + hallList.get(index).getAuthor() + "</td></tr>");
+                }
+            }
+            out.println("</tbody></table>");
+            out.println("</html></body>");
+        }
+        catch (Exception e)
+        {
+            out.println("error");
+        }
 
     }
 }
